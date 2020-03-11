1e206af8cdfb25d801fc815aba35d4f1b6680dcf
Amy Rippeto <amyrippeto@gmail.com>
2020-03-10 22:37:43 -0600

    Install dependencies

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////////
@@ -38,33 +40,44 @@ const replaceTemplate = require('./modules/replaceTemplate');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, {'Content-type': 'text/html'})
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML)
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
     res.end(output);
 
-  // Product page
+    // Product page
   } else if (pathname === '/product') {
-    res.writeHead(200, { 'Content-type': 'text/html' })
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
-    const output = replaceTemplate(tempProduct, product)
+    const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
-  // API
+    // API
   } else if (pathname === '/api') {
-    res.writeHead(200, {'Content-type': 'application/json'})
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
-  // Not found
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
@@ -77,6 +90,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
-
-
-
